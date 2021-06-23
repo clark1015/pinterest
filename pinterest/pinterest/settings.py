5d802f3929489ae54ff09d159fc4aba3d19141be
clark1015 <seok626898@gmail.com>
2021-06-23 21:52:30 +0900

    css seperation/ static

--- pinterest/pinterest/settings.py
+++ pinterest/pinterest/settings.py
@@ -123,7 +123,15 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
