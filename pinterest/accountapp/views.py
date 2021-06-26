7f696b1f04c96551529adac25bbc871c161aa695
clark1015 <seok626898@gmail.com>
2021-06-26 21:44:35 +0900

    input text and print at homepage

--- pinterest/accountapp/views.py
+++ pinterest/accountapp/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
+from .models import *
 
 
 # Create your views here.
 
 def hello_world(request):
-    return render(request, 'accountapp/hello_world.html')
+
+    if request.method == "POST":
+        text = request.POST.get('hello_world_input')
+
+        new_hello_world = HelloWorld()
+        new_hello_world.text = text
+        new_hello_world.save()
+        
+        
+        return render(request, 'accountapp/hello_world.html', {'text': new_hello_world.text})
+
+    else:
+        return render(request, 'accountapp/hello_world.html', {'text': 'GET METHOD!'})
     
