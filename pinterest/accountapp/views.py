61d4a28f37db60e3d4331a3075070a5b925c37b9
clark1015 <seok626898@gmail.com>
2021-07-26 14:50:49 +0900

    django course 21 commit

--- pinterest/accountapp/views.py
+++ pinterest/accountapp/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render
-from django.urls import reverse
-from .models import *
-from django.http import HttpResponse, HttpResponseRedirect
-from django.views.generic import CreateView
 from django.contrib.auth.models import User
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render
+from django.urls import reverse, reverse_lazy
+from django.views.generic import CreateView
+from django.contrib.auth.forms import UserCreationForm
+
+from .models import *
 
 # Create your views here.
 
@@ -25,5 +27,8 @@ def hello_world(request):
         return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
     
 
-class AccountCreateView(Createview):
+class AccountCreateView(CreateView):
     model = User
+    form_class = UserCreationForm
+    success_url = reverse_lazy('accountapp:hello_wrold')
+    template_name = 'accountapp/create.html'
