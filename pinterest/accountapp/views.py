aa14480b7726d70684812532923e39142123080f
clark1015 <seok626898@gmail.com>
2021-08-13 17:31:19 +0900

    django course 24 commit

--- pinterest/accountapp/views.py
+++ pinterest/accountapp/views.py
@@ -1,11 +1,12 @@
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse, reverse_lazy
-from django.views.generic import CreateView
-from django.contrib.auth.forms import UserCreationForm
+from django.views.generic import CreateView, DetailView
 
 from .models import *
+
 
 # Create your views here.
 
@@ -32,4 +33,9 @@ class AccountCreateView(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/create.html'
-    
+
+
+class AccountDetailView(DetailView):
+    model = User
+    context_object_name = 'target_user'
+    template_name = 'accountapp/detail.html'
