582af7f9f0c9bfe5c49d9a96fe0780d43e9c6835
clark1015 <seok626898@gmail.com>
2021-08-17 11:03:14 +0900

    django course 25 commit

---
+++ pinterest/accountapp/forms.py
@@ -0,0 +1,9 @@
+from django.contrib.auth.forms import UserCreationForm
+
+class AccountUpdateForm(UserCreationForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.fields['username'].disabled = True
+        #username의 칸을 비활성화 해주는 코드
+
