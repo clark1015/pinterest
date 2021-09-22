ac778d224350171ae26d0b760ba740e6a12d675f
clark1015 <seok626898@gmail.com>
2021-09-22 22:10:29 +0900

    django course 31

--- pinterest/profileapp/models.py
+++ pinterest/profileapp/models.py
@@ -5,11 +5,8 @@ from django.db import models
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
-    #유저가 탈되하면 이도 사라짐 -->cascade
     image = models.ImageField(upload_to='profile/', null=True)
-    #media root 밑에 profile이라는 경로가 생겨서 이에 저장됨
     nickname = models.CharField(max_length=20, unique=True, null=True)
-    #unique 유일한 닉네임을 설정
     message = models.CharField(max_length=100, null=True)
 
 
--- pinterest/profileapp/urls.py
+++ pinterest/profileapp/urls.py
@@ -1,6 +1,10 @@
+from django.urls import path
+
+from .views import *
 
 app_name = 'profileapp'
 
 urlpatterns = [
+    path('create/', ProfileCreateView.as_view(), name='create'),
 
 ]
